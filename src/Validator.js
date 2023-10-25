5a3d833fcca3fe3c18290094528adac7e4ac7ef7
michael <michail194@mail.ru>
2023-10-25 16:30:38 +0300

    sol before 4 step

---
+++ src/Validator.js
@@ -0,0 +1,14 @@
+/* eslint-disable class-methods-use-this */
+import ArraySchema from './ArraySchema.js';
+import NumberSchema from './NumberSchema.js';
+
+export default class Validator {
+  number() {
+    return new NumberSchema();
+  }
+
+  array() {
+    return new ArraySchema();
+  }
+
+}
